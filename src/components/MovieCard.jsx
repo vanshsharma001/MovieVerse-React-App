cea4c532984a7218e595dac65d6a7631dfff22b1
Vansh Sharma <vanshsharma252005@gmail.com>
2025-07-08 13:56:58 +0530

    Movie app

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-const MovieCard = ({ movie:
-  { title, vote_average, poster_path, release_date, original_language }
-}) => {
+const MovieCard = ({ movie: { Title, Year, Poster, imdbRating, Language, Type } }) => {
   return (
     <div className="movie-card">
       <img
-        src={poster_path ?
-          `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
-        alt={title}
+        src={Poster !== 'N/A' ? Poster : '/no-movie.png'}
+        alt={Title}
       />
 
       <div className="mt-4">
-        <h3>{title}</h3>
+        <h3>{Title}</h3>
 
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+            <p>{imdbRating !== 'N/A' ? imdbRating : 'N/A'}</p>
           </div>
 
           <span>•</span>
-          <p className="lang">{original_language}</p>
+          <p className="lang">{Language || Type}</p>
 
           <span>•</span>
-          <p className="year">
-            {release_date ? release_date.split('-')[0] : 'N/A'}
-          </p>
+          <p className="year">{Year}</p>
         </div>
       </div>
     </div>
-  )
-}
-export default MovieCard
+  );
+};
+
+export default MovieCard;
